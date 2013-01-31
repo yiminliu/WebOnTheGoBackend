b321aaad26d516be81b9157ba4c011899da8cc63
YLiu <YLiu@WCALA028.us.pointecom.net>
2013-01-30 16:47:22 -0800

    code refactory

--- src/com/tc/bu/WebOnTheGoBackend.java
+++ src/com/tc/bu/WebOnTheGoBackend.java
@@ -50,11 +50,8 @@ public class WebOnTheGoBackend {
   private static final SimpleDateFormat legibleDate = new SimpleDateFormat("MM/dd/yyyy");
   private static final DecimalFormat df = new DecimalFormat("0.00");
 
-  private TSCPMVNAService service;
-  private TSCPMVNA port;
-  
-  private com.tscp.mvne.Account tscpMvneAccount;
-  private NetworkInfo networkInfo;
+  private static TSCPMVNAService service;
+  private static TSCPMVNA port;
   
   public WebOnTheGoBackend() {
     init();
@@ -63,7 +60,7 @@ public class WebOnTheGoBackend {
   public static void main(String[] args) {
 		WebOnTheGoBackend wotgbe = new WebOnTheGoBackend();
 		int acctNo = -100;
-		logger.info("Start topup charge process.");
+		logger.info("********** Start topup charge process *************");
 	    try {
 	      if(acctNo < 0) 
 	    	  wotgbe.chargeAccounts(wotgbe.getAccountToChargeList());
@@ -72,16 +69,16 @@ public class WebOnTheGoBackend {
 	    } 
 	    catch (WebServiceException wsException) {
 	      if (wsException.getMessage().indexOf("Attempted to read or write protected memory") >= 0) {
-	        System.err.println("Memory corrupt. Exiting the process.");
+	        logger.error("Memory corrupt. Exiting the process{}.", wsException.getMessage());
 	        System.exit(1);
 	      }
 	      else
-	    	logger.error(wsException.getMessage()) ;	      
+	    	logger.error("Error occured in the topup process {}.", wsException.getMessage()) ;	      
 	    }
 	    catch (Exception exception) {
-		   	logger.error(exception.getMessage()) ;	      
+		   	logger.error("Error occured in the topup process {}.", exception.getMessage()) ;	      
 	    }
-	    logger.info("Finished topup charge process.");
+	    logger.info("********** Finished topup charge process **********");
 	    System.out.println("");
   }
   
@@ -93,40 +90,24 @@ public class WebOnTheGoBackend {
    */
   private void chargeAccounts(List<Account> accountList) {
 	
-	com.tscp.mvne.Account tscpMvneAccount = new com.tscp.mvne.Account();
 	Customer customer = null;	  
-    for (Account account : accountList) {
+	com.tscp.mvne.Account tscpMvneAccount = null;
+	for (Account account : accountList) {
       try {
           customer = getCustomerInfo(account);
+          tscpMvneAccount = getAccount(account.getAccountNo());
           int defaultPaymentId = getCustomerPaymentDefault(customer);            
           Double chargeAmount = determinCustomerTopUpAmount(customer, tscpMvneAccount);
           try {
               makePayment(customer, defaultPaymentId, tscpMvneAccount, null, df.format(chargeAmount));
           } 
-          catch (PaymentException payment_ex) {
-              logger.info("Sending failure notification to {}", EMAIL_ERROR);
-              String remainingBalance = tscpMvneAccount.getBalance();
-              CreditCard paymentInfo = getPaymentMethod(customer.getId(), defaultPaymentId);
-              String paymentMethod = determineCreditCardType(paymentInfo.getCreditCardNumber().substring(0, 1));
-              String body = EmailHelper.getPaymentFailureBody(tscpMvneAccount.getFirstname(), Integer.toString(tscpMvneAccount.getAccountNo()), account
-                  .getMdn(), "", chargeAmount.toString(), paymentMethod, paymentInfo.getCreditCardNumber(), legibleDate.format(new Date()), payment_ex
-                  .getMessage(), remainingBalance);
-              sendEmail(EMAIL_ERROR, "Error processing payment for Account " + tscpMvneAccount.getAccountNo(), body);
-              logger.debug("Email sent");
+          catch (PaymentException paymentEx) {
+        	  sendPaymentFailureNotification(tscpMvneAccount, customer, account, defaultPaymentId, chargeAmount, paymentEx); 
           }      
       } 
-      catch (CustomerException cust_ex) {
-            logger.warn("Skipping Account " + account.getAccountNo() + "Error was " + cust_ex.getMessage(), cust_ex);
-            ProcessException process_ex = new ProcessException("Payment Processing", cust_ex.getMessage(), cust_ex);
-            process_ex.setAccountNo(Integer.toString(tscpMvneAccount.getAccountNo()));
-            process_ex.setMdn(account.getMdn());
-            process_ex.setAccount(tscpMvneAccount);
-            process_ex.setNetworkInfo(networkInfo);
-            //throw process_ex;
-            logger.error("Error:: " + process_ex.getMessage() + ". Formerly we sent emails here...server is handling this action now. ");
-      } catch (ProcessException process_ex) {
-        // previously sent emails to the customer from here. This is now done in TSCPMVNE.TruConnect.submitPaymentById
-        logger.error("Error:: " + process_ex.getMessage() + ". Formerly we sent emails here...server is handling this action now. ");
+      catch (CustomerException custEx) {
+            logger.warn("Skipping Account " + account.getAccountNo() + "Error was " + custEx.getMessage(), custEx);
+            //we may need to notify the error to a technical group
       }
       logger.info("Done with Account {}", account.getAccountNo());
     }    
@@ -141,57 +122,42 @@ public class WebOnTheGoBackend {
 	  chargeAccounts(accountList);
   }	
   
-  private Customer getCustomerInfo(Account account) throws ProcessException {
+  private Customer getCustomerInfo(Account account) throws CustomerException {
 	Customer customer = null;  
-    ProcessException process_ex = null;
     try {
-      logger.info("Getting Customer Info for Account " + account.getAccountNo());
-      customer = getCustomerFromAccount(account.getAccountNo());
-      if (customer.getId() == 0) {
-        throw new CustomerException("Customer Info not be found for Account " + account.getAccountNo());
-      }
-    } catch (CustomerException cust_ex) {
-      process_ex = new ProcessException("CustomerInformation Retrieval", cust_ex);
-    }
-    try {
-      logger.debug("Creating TSCPMVNE.Account Object for customer " + customer.getId());
-      tscpMvneAccount = getAccount(account.getAccountNo());
-    } catch (CustomerException cust_ex) {
-      if (process_ex == null) {
-        process_ex = new ProcessException("AccountInformation Retrieval", cust_ex);
-      }
-    }
-    if (process_ex != null) {
-      process_ex.setAccountNo(Integer.toString(account.getAccountNo()));
-      process_ex.setMdn(account.getMdn());
-      process_ex.setAccount(tscpMvneAccount);
-      process_ex.setNetworkInfo(networkInfo);
-      throw process_ex;
+       logger.debug("Getting Customer Info for Account " + account.getAccountNo());
+       customer = getCustomerFromAccount(account.getAccountNo());
+       if (customer.getId() == 0) {
+          throw new CustomerException("Customer Info not be found for Account " + account.getAccountNo());
+       }
+    } 
+    catch (CustomerException custEx) {
+      throw custEx;	
     }
     return customer;
   }
-
+    
   private CreditCard getPaymentMethod(int custId, int pmtId) throws CustomerException {
-    CreditCard creditCard = port.getCreditCardDetail(pmtId);
-    if (creditCard == null || creditCard.getCreditCardNumber() == null || creditCard.getCreditCardNumber().trim().length() == 0) {
-      throw new CustomerException("Error retrieving Credit Card for customer " + custId + " pmt " + pmtId);
-    }
-    return creditCard;
+     CreditCard creditCard = port.getCreditCardDetail(pmtId);
+     if (creditCard == null || creditCard.getCreditCardNumber() == null || creditCard.getCreditCardNumber().trim().length() == 0) {
+         throw new CustomerException("Error retrieving Credit Card for customer " + custId + " pmt " + pmtId);
+     }
+     return creditCard;
   }
 
   private com.tscp.mvne.Account getAccount(int accountNo) throws CustomerException {
-    com.tscp.mvne.Account account = port.getAccountInfo(accountNo);
-    if (account == null) {
-      throw new CustomerException("Error fetching Account " + accountNo);
-    } else if (account.getContactEmail() == null || account.getContactEmail().trim().length() == 0) {
-      throw new CustomerException("Error fetching Email Address for account " + account.getAccountNo());
-    }
-    return account;
+     com.tscp.mvne.Account account = port.getAccountInfo(accountNo);
+     if (account == null) {
+        throw new CustomerException("Error fetching Account " + accountNo);
+     } 
+     else if (account.getContactEmail() == null || account.getContactEmail().trim().length() == 0) {
+        throw new CustomerException("Error fetching Email Address for account " + account.getAccountNo());
+     }
+     return account;
   }
   
   private Double determinCustomerTopUpAmount(Customer customer, com.tscp.mvne.Account tscpMvneAccount) throws CustomerException {
-	    
-		logger.debug("Getting top-up amount for customer {}", customer.getId());  
+	    logger.info("Getting top-up amount for customer {}", customer.getId());  
 		CustTopUp custTopUp = new CustTopUp();
 	    custTopUp = port.getCustTopUpAmount(customer, tscpMvneAccount);
 	    if (custTopUp == null || custTopUp.getTopupAmount() == null || custTopUp.getTopupAmount().trim().length() == 0) {
@@ -206,58 +172,54 @@ public class WebOnTheGoBackend {
 	        tscpMvneAccount.setBalance(Double.toString(Double.parseDouble(tscpMvneAccount.getBalance()) + Double.parseDouble(custTopUp.getTopupAmount())));
 	    }
 	    Double chargeAmount = Double.parseDouble(custTopUp.getTopupAmount()) * topUpQuantity;
-	    logger.debug("Customer will be topped up. Total charge is {}.", topUpQuantity, NumberFormat.getCurrencyInstance().format(chargeAmount));
+	    logger.info("Customer will be topped up. Total charge is {}.", topUpQuantity, NumberFormat.getCurrencyInstance().format(chargeAmount));
 	    return chargeAmount;
   }
 
   private int getCustomerPaymentDefault(Customer customer) throws CustomerException {
-    // PaymentInformation paymentInfo = new PaymentInformation();
-	logger.debug("Getting default payment method for customer {}", customer.getId());  
-    int paymentId = 0;
-    List<CustPmtMap> custPaymentMap = port.getCustPaymentList(customer.getId(), 0);
-    if (custPaymentMap != null && custPaymentMap.size() > 0) {
-      paymentId = custPaymentMap.get(0).getPaymentid();
-    } else {
+     logger.debug("Getting default payment method for customer {}", customer.getId());  
+     int paymentId = 0;
+     List<CustPmtMap> custPaymentMap = port.getCustPaymentList(customer.getId(), 0);
+     if (custPaymentMap != null && custPaymentMap.size() > 0) {
+         paymentId = custPaymentMap.get(0).getPaymentid();
+     } 
+     else {
       throw new CustomerException("Error retrieving Payments for Customer " + customer.getId());
-    }
-    return paymentId;
+     }
+     return paymentId;
   }
 
-  private Customer getCustomerFromAccount(int accountno) throws ProcessException {
-    Customer customer = new Customer();
-    try {
-      customer.setId(port.getCustFromAccount(accountno).getCustId());
-    } catch (NullPointerException np_ex) {
-      logger.info(np_ex.getMessage());
-      throw new ProcessException("CustomerRetrieval", "Unable to get customer information from map against account " + accountno);
-    }
-    if (customer.getId() == 0) {
-      logger.info("Customer information from map against account " + accountno + " returned a 0 CustID");
-      throw new ProcessException("CustomerRetrieval", "Customer information from map against account " + accountno + " returned a 0 CustID");
-    }
-    return customer;
+  private Customer getCustomerFromAccount(int accountNo) throws CustomerException {
+     Customer customer = new Customer();
+     customer.setId(port.getCustFromAccount(accountNo).getCustId());
+     if (customer.getId() == 0) {
+       logger.error("Customer information from map against account " + accountNo + " returned a 0 CustID");
+       throw new CustomerException("Customer information from map against account " + accountNo + " returned a 0 CustID");
+     }
+     return customer;
   }
 
   private void makePayment(Customer customer, int paymentId, com.tscp.mvne.Account account, CreditCard creditCard, String amount)
       throws PaymentException {
-    logger.info("Making payment for CustomerId " + customer.getId() + " against Pmt ID " + paymentId + " in the Amount of $" + df.format(amount) + ".");
-    String sessionid = "CID" + customer.getId() + "T" + getTimeStamp() + "AUTO";
-    PaymentUnitResponse response = null;
-    try {
+     //logger.info("Making payment for CustomerId " + customer.getId() + " against Pmt ID " + paymentId + " in the Amount of $" + df.format(amount) + ".");
+     logger.info("Making payment for account {}, in the Amount of ${}. ", account.getAccountNo(), df.format(amount));
+     String sessionid = "CID" + customer.getId() + "T" + getTimeStamp() + "AUTO";
+     PaymentUnitResponse response = null;
+     try {
         response = port.submitPaymentByPaymentId(sessionid, customer, paymentId, account, amount);
-    } 
-    catch (WebServiceException wse) {
-      logger.warn("WebService Exception thrown :: " + wse.getMessage());
-      // will catch this exception at main()
-      if (wse.getMessage().indexOf("Attempted to read or write protected memory") >= 0) {
-        throw wse;
-      }
-      if (wse.getCause() != null) {
-        logger.warn("Immediate WSException Cause was :: " + wse.getCause().getMessage());
-      }
-      throw new PaymentException(wse.getMessage());
-    }
-    if (response != null) {
+     } 
+     catch (WebServiceException wse) {
+       logger.warn("WebService Exception thrown {} ", wse.getMessage());
+       // will catch this exception at main()
+       if (wse.getMessage().indexOf("Attempted to read or write protected memory") >= 0) {
+         throw wse;
+       }
+       if (wse.getCause() != null) {
+         logger.warn("Immediate WSException Cause was :: " + wse.getCause().getMessage());
+       }
+       throw new PaymentException(wse.getMessage());
+     }
+     if (response != null) {
         logger.info("PaymentUnit Response ");
         logger.info("AuthCode   :: " + response.getAuthcode());
         logger.info("ConfCode   :: " + response.getConfcode());
@@ -271,23 +233,23 @@ public class WebOnTheGoBackend {
   }
 
   private List<Account> getAccountToChargeList() {
-    logger.info("Fetching accounts to charge...");
-    List<Account> accountList = null;
-    Session session = null;
-    try {
+     logger.info("Fetching accounts to charge...");
+     List<Account> accountList = null;
+     Session session = null;
+     try {
     	session = HibernateUtil.getSessionFactory().getCurrentSession();
         session.beginTransaction();
         Query q = session.getNamedQuery("sp_fetch_accts_to_charge");
         accountList = q.list();
-        if(accountList == null)
+        if(accountList == null || accountList.isEmpty())
         	logger.info("   ...{} accounts will be topped-up", 0);
         else 
             logger.info("   ...{} accounts will be topped-up", accountList.size());
         session.getTransaction().commit();
-    }   
-    catch(HibernateException e){
-    	//session.getTransaction().rollback();
-    	logger.error("Failed on executing sp_fetch_accts_to_charge, due to: " +e.getMessage());
+     }   
+     catch(HibernateException e){
+    	session.getTransaction().rollback();
+    	logger.error("Failed on executing sp_fetch_accts_to_charge, due to {} ", e.getMessage());
     }
     return accountList;
   }
@@ -321,26 +283,46 @@ public class WebOnTheGoBackend {
 		  service = new TSCPMVNAService(new URL(wsdlLocation), new QName(nameSpace, serviceName));
 		  port = service.getTSCPMVNAPort();
 	  } 
-	  catch (MalformedURLException url_ex) {
-	      logger.error("Unable to reach webservice. {}", url_ex.getMessage());
+	  catch (MalformedURLException urlEx) {
+	      logger.error("Unable to reach webservice. {}", urlEx.getMessage());
+	      System.exit(1);
 	  }	  
   }
   
   private static String getTimeStamp() {
-    return sdf.format(new Date());
+     return sdf.format(new Date());
   }  
   
+  private void sendPaymentFailureNotification(com.tscp.mvne.Account tscpMvneAccount, Customer customer, Account account,
+		                                      int defaultPaymentId, Double chargeAmount, PaymentException paymentEx) 
+                                              throws CustomerException {
+     logger.info("Sending failure notification to {}", EMAIL_ERROR);
+     String remainingBalance = tscpMvneAccount.getBalance();
+     CreditCard paymentInfo = getPaymentMethod(customer.getId(), defaultPaymentId);
+     String paymentMethod = determineCreditCardType(paymentInfo.getCreditCardNumber().substring(0, 1));
+     String body = EmailHelper.getPaymentFailureBody(tscpMvneAccount.getFirstname(), 
+    		                                         Integer.toString(tscpMvneAccount.getAccountNo()), 
+    		                                         account.getMdn(), "", chargeAmount.toString(), 
+    		                                         paymentMethod, paymentInfo.getCreditCardNumber(), 
+    		                                         legibleDate.format(new Date()), 
+    		                                         paymentEx.getMessage(), 
+    		                                         remainingBalance);
+     sendEmail(EMAIL_ERROR, "Error processing payment for Account " + tscpMvneAccount.getAccountNo(), body);
+     logger.info("Email sent");
+  }
+  
   private void sendEmail(String emailAddress, String subject, String body) {
-	    MailClient mail = new MailClient();
-	    Vector<Recipient> recipients = new Vector<Recipient>();
-	    Recipient recipient = new Recipient();
-	    recipient.setEmailAddress(emailAddress);
-	    recipients.add(recipient);
-	    try {
-	      body = EmailHelper.getEmailHeader() + body + EmailHelper.getEmailFooter();
-	      mail.postMail(recipients, subject, body, MailClient.SYSTEM_SENDER);
-	    } catch (Exception ex) {
-	      ex.printStackTrace();
-	    }
-	  }
+	 MailClient mail = new MailClient();
+	 Vector<Recipient> recipients = new Vector<Recipient>();
+	 Recipient recipient = new Recipient();
+	 recipient.setEmailAddress(emailAddress);
+	 recipients.add(recipient);
+	 try {
+	     body = EmailHelper.getEmailHeader() + body + EmailHelper.getEmailFooter();
+	     mail.postMail(recipients, subject, body, MailClient.SYSTEM_SENDER);
+	 } 
+	 catch (Exception ex) {
+	     logger.error("Error occured while sending notification email, due to {}", ex.getMessage());
+	 }
+  }
 }
